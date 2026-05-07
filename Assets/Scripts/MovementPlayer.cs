c5b9eba67534a6720e53792873df40b3052b91e4
DeonsGaming <Deon.mob@gmail.com>
2026-05-07 11:51:17 +0200

    added a test player and player movement
    test player will be replaced and the normal player will be placed and also the test floor will be removed

---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementPlayer : MonoBehaviour
+{
+    [SerializeField] private float _jumpPower = 3f;
+    [SerializeField] private float _walkSpeed = 3f;
+    [SerializeField] private PlayerInput _playerInput;
+    private void Start()
+    {
+        if (_playerInput == null)
+            _playerInput.OnPlayerInputRecieve.AddListener(MovePlayerSideWays);
+    }
+    public void MovePlayerSideWays(Vector2 direction)
+    {
+        transform.position += new Vector3(direction.x, direction.y, 0) * _walkSpeed * Time.deltaTime;
+    }
+    public void MovePlayerUp(Vector2 direction)
+    {
+        transform.position += new Vector3(direction.x, direction.y, 0) * _jumpPower * Time.deltaTime;
+    }
+}
