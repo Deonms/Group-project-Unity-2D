4b96529632b4a216baa1e4e357da648462c9f967
Y.Ouallal <2235702@talnet.nl>
2026-06-05 10:50:18 +0200

    Happy Bar Added and More
    The Happy Bar has been added along with the Money System showing how much money the player has.

--- Assets/Scripts/GeldSysteem.cs
+++ Assets/Scripts/GeldSysteem.cs
@@ -1,12 +1,20 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GeldSysteem : MonoBehaviour
 {
     [SerializeField] private float _geldValue;
+    [SerializeField] private TMP_Text _geldText;
 
     public float GetGeld()
     {
         return _geldValue;
+    }
+
+    private void Start()
+    {
+        _geldText.text = "€ " + _geldValue;
     }
 
     public void GeldErafHalen(float ProductKost)
